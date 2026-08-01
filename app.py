d88b66c3df8ce59bdf1934f01215e73b195ee36a
Dalima <djl6365@psu.edu>
2026-08-01 13:48:58 -0400

    updated app.py to build the Flask backend

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+from chatbot import generate_response
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_message = request.json["message"]
+
+    response = generate_response(user_message)
+
+    return jsonify({
+        "response": response
+    })
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
